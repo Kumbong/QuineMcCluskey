c30c4a85a5708f8efef5160fa72cfdd55628882f
Kumbong Hermann Nyuykonge <kumbonghermann@gmail.com>
2019-06-13 22:40:24 +0000

    tests

--- src/tests/test_quine_mccluskey.py
+++ src/tests/test_quine_mccluskey.py
@@ -1,2 +1,32 @@
 from unittest import TestCase, main
 
+class TestQM(TestCase):
+    def setup(self):
+        pass
+
+    def test_to_binary(self):
+        pass
+
+    def test_combine(self):
+        pass
+
+    def test_combine_groups(self):
+        pass
+
+    def test_combine_generation(self):
+        pass
+
+    def test_group_minterms(self):
+        pass
+
+    def test_pis(self):
+        pass
+
+    def test_can_cover(self):
+        pass
+    
+    def test_epis(self):
+        pass
+
+    def test_other_pis(self):
+        pass
