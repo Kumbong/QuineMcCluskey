ec2afb69439d00cf4442692a6a0b5d5b749878c4
Kumbong Hermann Nyuykonge <kumbonghermann@gmail.com>
2019-06-23 23:17:27 +0000

    update tests

--- core/tests/test_quine_mccluskey.py
+++ core/tests/test_quine_mccluskey.py
@@ -6,7 +6,21 @@ class TestQM(TestCase):
         self.fail()
 
     def test_to_binary(self):
-        self.fail()
+        
+        #test if the strings are the actual binary representations
+        #for each of the strings
+        minterms = [1,2,3,4,5,6,15]
+        qm =  QM(minterms)
+        expected_conversion = ['0001','0010','0011','0100','0101','0110','1111']
+        conversion = qm.to_binary(minterms)
+        self.assertEqual(conversion,expected_conversion)
+        
+        #check if the number of bits in each binary string is the same for 
+        #all
+        for term in conversion:
+            self.assertEqual(len(term),4)
+
+        #check if the same applies to the don't cares
 
     def test_combine(self):
         self.fail()
